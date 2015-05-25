f294754ffa5bf4c5ceb4edb063ddfa62c5716ba1
sammous <moustachir.sami@gmail.com>
2015-05-26 01:44:42 +0200

    switching to mysql 2

--- app.js
+++ app.js
@@ -2,11 +2,95 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 
-var configDB = require('./config/database.js');
 
+// custom libraries
+// routes
+var route = require('./routes');
+// model
+var Model = require('./models/user');
+
+app.set('view engine','ejs');
+
+//app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true}));
+
+passport.use(new LocalStrategy(function(username, password, done) {
+   new Model.User({username: username}).fetch().then(function(data) {
+      var user = data;
+      if(user === null) {
+         return done(null, false, {message: 'Invalid username or password'});
+      } else {
+         user = data.toJSON();
+         if(!bcrypt.compareSync(password, user.password)) {
+            return done(null, false, {message: 'Invalid username or password'});
+         } else {
+            return done(null, user);
+         }
+      }
+   });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+   new Model.User({username: username}).fetch().then(function(user) {
+      done(null, user);
+   });
+});
+
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({secret: 'secret strategic xxzzz code'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/', route.signIn);
+
+// signin
+// GET
+app.get('/signin', route.signIn);
+// POST
+app.post('/signin', route.signInPost);
+
+// signup
+// GET
+app.get('/signup', route.signUp);
+// POST
+app.post('/signup', route.signUpPost);
+
+// logout
+// GET
+app.get('/logout', route.logout);
+
+/********************************/
+
+/********************************/
+// 404 not found
+app.use(route.notFound404);
+
+
+var server = app.listen(app.get('port'), function(err) {
+   if(err) throw err;
+
+   var message = 'Server is running @ http://localhost:' + server.address().port;
+   console.log(message);
+});
+
+/*
 // connect to mongo
 mongoose.connect('mongodb://localhost/newauth');
 
@@ -22,18 +106,25 @@ app.get('/', function(req, res) {
   res.render('signin.ejs');
 });
 
-app.post('/', function(req, res){
-  User.findOne({ username: req.body.username }, function(err, user) {
-    if (!user) {
-      res.render('signin.ejs', {error: 'Invalid username or password.' });
-    } else {
-      if (req.body.password == user.password ){
-        res.redirect('/dashboard');
-      } else {
-        res.render('signin.ejs', {error: 'Invalid username or password.' });
+app.post('/', function(req, res, next) {
+   passport.authenticate('local', { successRedirect: '/',
+                          failureRedirect: '/signin'}, function(err, user, info) {
+      if(err) {
+         return res.render('signin', {title: 'Sign In', errorMessage: err.message});
       }
-    }
-  });
+
+      if(!user) {
+         return res.render('signin', {title: 'Sign In', errorMessage: info.message});
+      }
+      return req.logIn(user, function(err) {
+         if(err) {
+            return res.render('signin', {title: 'Sign In', errorMessage: err.message});
+         } else {
+            return res.redirect('/');
+         }
+      });
+   })(req, res, next);
+};
 });
 
 app.get('/logout', function(req, res){
@@ -57,5 +148,5 @@ function isLoggedIn(req, res, next) {
     // if they arent redirect them to the home page
     res.redirect('/');
 }
-
+*/
 app.listen(port);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,19 @@
-var DB = require('./db').DB;
 
-var User = DB.Model.extend({
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : 'root',
+    database : 'dbUsers',
+    charset  : 'utf8'
+  }
+});
+
+var bookshelf = require('bookshelf')(knex);
+
+var User = bookshelf.Model.extend({
    tableName: 'tblUsers',
    idAttribute: 'userId',
 });
