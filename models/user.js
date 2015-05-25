b3c421d79c0b73118db877bf2d0ec45469972642
sammous <moustachir.sami@gmail.com>
2015-05-26 00:38:42 +0200

    switching to mysql

--- models/user.js
+++ models/user.js
@@ -1,22 +1,10 @@
-var mongoose = require('mongoose');
+var DB = require('./db').DB;
 
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+var User = DB.Model.extend({
+   tableName: 'tblUsers',
+   idAttribute: 'userId',
+});
 
-// model User
-var User = mongoose.model('User', new Schema({
-  id: ObjectId,
-  username: { type: String, unique: true},
-  password: String,
-}));
-
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+module.exports = {
+   User: User
 };
