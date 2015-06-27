1dcc34a36153df67abee4fdf599cae1b3d6500f1
sammous <moustachir.sami@gmail.com>
2015-06-27 23:46:21 +0200

    minor updates administration

--- app/routes.js
+++ app/routes.js
@@ -46,15 +46,15 @@ module.exports = function(app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	app.get('/signup', isLoggedIn, function(req, res) {
+	app.get('/admin/signup', isLoggedIn, isAdmin, function(req, res) {
 		// render the page and pass in any flash data if it exists
 		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
 
 	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/signup', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	app.post('/admin/signup', passport.authenticate('local-signup', {
+		successRedirect : '/admin/signup', // redirect to the secure profile section
+		failureRedirect : '/admin/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	})
 	);
